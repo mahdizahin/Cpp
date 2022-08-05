28f670f435a5b40ba2e055fdd54ced61b201eca5
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-08-06 02:14:15 +0600

    Add files via upload

---
+++ Setprecision/setprecision.cpp
@@ -0,0 +1,35 @@
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main ()
+{
+    float n1=45345, n2=76, s;
+
+
+
+    //cin>>n1>>n2;
+
+
+    cout<<noshowpoint;
+    cout<<fixed;
+    cout<<setprecision(25);
+
+
+    s = n1+n2;
+    cout<<setw(40)<<"What will bw print"<<s<<endl;
+
+    s = n1-n2;
+    cout<<setw<<s<<endl;
+
+    s = n1*n2;
+    cout<<setw<<s<<endl;
+
+    s = n1/n2;
+    cout<<setw<<s<<endl;
+
+    //s = n1%n2;
+   // cout<<s<<endl;
+
+}
+
