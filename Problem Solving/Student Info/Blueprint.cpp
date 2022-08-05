59ea89b430d3998128f954767e874943de5ecbf3
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-08-06 02:27:26 +0600

    Add files via upload

---
+++ Problem Solving/Student Info/Blueprint.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+
+class Person
+{
+
+};
+
+
+class Employee : public Person
+{
+
+};
+
+class Student : public Person
+{
+
+};
+
+class Student : public Person
+{
+
+};
+
+
+int main ()
+{
+
+}
