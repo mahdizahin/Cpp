56c807d1b1acaac4232945506364b0a74d68c380
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-08-06 20:36:25 +0600

    Add files via upload

---
+++ String/Practice1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+
+ char a,b,c,d,e,f,g,h,i;
+ a='Z';
+ cout<<a;
+
+b='A';
+ cout<<b;
+
+c='H';
+ cout<<c;
+
+d='I';
+ cout<<d;
+e='N';
+ cout<<e;
+f=' ';
+ cout<<f;
+g='X';
+ cout<<g;
+h='X';
+ cout<<h;
+i='X';
+ cout<<i;
+
+return 0;
+ }
