9b0ecb1b6df5d19b332ccbbe9ddbdf56c0e0aebd
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-11-19 23:28:45 +0600

    Add files via upload

---
+++ Switch-Case/Sw 1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+
+switch(n){
+
+case 1:
+    cout<<"why did you press one? ";
+    break;
+case 2:
+    cout<<"why did you press two? ";
+    break;
+case 3:
+    cout<<"why did you press three? ";
+    break;
+case 4:
+    cout<<"why did you press four? ";
+    break;
+
+default :
+    cout<<"press right number";
+    break;
+
+}
+
+
+}
