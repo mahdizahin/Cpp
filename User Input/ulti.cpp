a7daaac240c9f9f0a2f42f38b0ae327b1d50f830
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-06-28 09:54:51 -0700

    Add files via upload

---
+++ User Input/ulti.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+string name;
+int rating;
+
+cout<<"Enter name or id : ";
+cin>>name;
+cout<<"Rate this guy : ";
+cin>>rating;
+
+
+cout<<name<<" Rating is : "<<rating;
+
+}
